0c37190737d1442a5771d6f13f4230b69e4df5f9
Pablo Gonzalez <pglez82@gmail.com>
2021-01-16 10:44:23 +0100

    fixing a bug

--- webapp/src/api/api.js
+++ webapp/src/api/api.js
@@ -1,7 +1,7 @@
 
 //REACT_APP_API_URI is an enviroment variable defined in the file .env.development or .env.production
 export async function addUser(username,email){
-    const apiEndPoint= process.env.REACT_APP_API_URI
+    const apiEndPoint= process.env.REACT_APP_API_URI || 'http://localhost:5000/api'
     let response = await fetch(apiEndPoint+'/users/add', {
         method: 'POST',
         headers: {'Content-Type':'application/json'},
@@ -11,8 +11,7 @@ export async function addUser(username,email){
 }
 
 export async function getUsers(){
-    const apiEndPoint= process.env.REACT_APP_API_URI
-    console.log(process.env.REACT_APP_API_URI)
+    const apiEndPoint= process.env.REACT_APP_API_URI || 'http://localhost:5000/api'
     console.log(apiEndPoint)
     let response = await fetch(apiEndPoint+'/users/list')
     return await response.json()
