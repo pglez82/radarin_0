0b7fc6e9d59c0321033f6411776a82a558f8c9f2
Pablo Gonzalez <pglez82@gmail.com>
2021-01-21 15:30:48 +0100

    e2e

--- restapi/tests/server-for-tests.js
+++ restapi/tests/server-for-tests.js
@@ -13,13 +13,16 @@ const api = require("../api")
 
 
 module.exports.startdb = async () => {
-    mongod = new MongoMemoryServer({ instance: { port: 27017}})
+    mongod = new MongoMemoryServer({ instance: { port: 27017,dbName: 'testdb'}})
     const mongo_uri =await mongod.getUri();
     console.log(mongo_uri)
-    await mongoose.connect(mongo_uri, { useNewUrlParser: true,useUnifiedTopology: true })
+    
 }
 
 module.exports.startserver = async () => {
+    console.log("conecceting to database")
+    await mongoose.connect("mongodb://127.0.0.1:27017/testdb?", { useNewUrlParser: true,useUnifiedTopology: true })
+    console.log("connected")
     app = express()
 
     app.use(cors());
@@ -33,12 +36,12 @@ module.exports.startserver = async () => {
 }
 
 module.exports.closeServer = async () => {
+    await mongoose.connection.dropDatabase();
+    await mongoose.connection.close();
     await server.close()
 }
 
 module.exports.closeDB = async () => {
-    await mongoose.connection.dropDatabase();
-    await mongoose.connection.close();
     await mongod.stop();
 }
 
