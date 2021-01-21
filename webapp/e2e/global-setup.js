622af0c314b549171c4c356a9ac7f12783b09f07
Pablo Gonzalez <pglez82@gmail.com>
2021-01-21 19:00:50 +0100

    trying to finish e2e tests

--- webapp/e2e/global-setup.js
+++ webapp/e2e/global-setup.js
@@ -3,19 +3,19 @@ module.exports = async () => {
     await setupDevServer([
     {
         command: 'node start-db.js',
-        launchTimeout: 30000,
+        launchTimeout: 100000,
         debug:true,
         port: 27017,
     },
     {
         command: 'node start-restapi.js',
-        launchTimeout: 30000,
+        launchTimeout: 60000,
         debug:true,
         port: 5000,
     },
     {
         command: 'BROWSER=none npm start',
-        launchTimeout: 30000,
+        launchTimeout: 60000,
         debug: true,
         port: 3000
     }])
